ec7e7d32f2d69651b6cb6bd38f7756c93f0c8f7d
aniketbhuse <aniketbhuse21@gmail.com>
2021-10-21 19:30:08 +0530

    Add project files.

---
+++ EmployeeMangSystem/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeMangSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Desgination { get; set; }
+
+        public string Salary { get; set; }
+    }
+}
